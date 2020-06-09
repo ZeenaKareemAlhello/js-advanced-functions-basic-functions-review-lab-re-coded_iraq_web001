25f5d54f84e83a643d24e37846b7c5d58dad2c90
zeena kareem <zena34269@gmail.com>
2020-06-09 16:48:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+
+function saturdayFun(task="roller-skate"){
+  return `This Saturday, I want to ${task}!`
+}
+
+saturdayFun()
+                          //="go to the office"
+let mondayWork=(function (task2){
+  return `This Monday, I will ${task2} `
+})("go to the office")
+mondayWork()
+
+function wrapAdjective(){
+  return function(var1){
+    return let encouragingPromptFunction = wrapAdjective("!!!")
+  }("special")
+}
+
+
+
+
+
+
+
+
+
+
+
